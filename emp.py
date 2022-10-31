cfe4035c9ef4693bd0cd263d9bf7a08630c9eefa
hugjph <hughjohnson2018@outlook.com>
2022-10-31 18:35:07 +0000

    exercise 1

---
+++ emp.py
@@ -0,0 +1,6 @@
+class Employee():
+    def __init__(self, id_number, employee_name, department, title):
+        self.__id_number = id_number
+        self.__employee_name = employee_name
+        self.__department = department
+        self.__title = title
